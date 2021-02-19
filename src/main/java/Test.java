8bc79044a82782147e128a0e939460afff7fb5f6
shanx <shanx@atguigu.com>
2021-02-19 20:47:22 +0800

    first commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * description:
+ * created by Shan on 2021/2/19
+ * version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hhh");
+    }
+}
